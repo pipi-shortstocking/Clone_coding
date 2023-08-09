24115eecd02d82e26e84923f692aa797266c236b
pipi-shortstocking <jannie435@naver.com>
2023-08-09 16:22:56 +0900

    login part

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저의 기본 동작을 막음
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME) // 클래스 이름을 새로이 붙여줌
+    const typedUsername = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, typedUsername);
+    paintGreetings(typedUsername);
 }
 
-// 내부 함수는 직접 실행되는 것이 아님 브라우저가 실행과 동시에 event에 대한 정보도 담아줌
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit); // 내부 함수는 직접 실행되는 것이 아님 브라우저가 실행과 동시에 event에 대한 정보도 담아줌
+} else {
+    paintGreetings(savedUsername);
+}
